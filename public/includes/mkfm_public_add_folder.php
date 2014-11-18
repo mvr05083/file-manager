403171aa3b89194511b38ffb6aafb8c76a47f706
mvr05083 <mkdeveloper56@gmail.com>
2014-11-18 18:29:08 -0500

    small changes

--- public/includes/mkfm_public_add_folder.php
+++ public/includes/mkfm_public_add_folder.php
@@ -10,13 +10,12 @@ function mkfm_add_folder() {
     }
     
     $target_dir = mkfm_get_current_dir() . '/' . $folder_name;
-    if (is_dir($target_dir)){
-        echo json_encode("<div id='folder-message' class='alert-box warning'>Sorry, that directory already exists. Please select a different name</div>");
-//        echo json_encode($target_dir);
+    if ( is_dir( $target_dir ) ) {
+        echo json_encode( "<div id='folder-message' class='alert-box warning'>Sorry, that directory already exists. Please select a different name</div>" );
         die();
        
     } else {
-        if (mkdir($target_dir, 0755)){
+        if ( mkdir( $target_dir, 0755 ) ) {
             //TODO: create the utilities for calling the database
             //insertNew(createHash($target_dir), $target_dir, 1);
             echo json_encode( "<div id='folder-message' class='alert-box success'>". basename($target_dir) ." created.</div>" );
@@ -27,4 +26,3 @@ function mkfm_add_folder() {
         }
     }
 }
-
